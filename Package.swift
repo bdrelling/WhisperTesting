c7c6251b4bef818478c1cb577130d83e8630a949
Brian Drelling <brian@drelling.com>
2022-12-30 18:21:59 -0600

    Added WhisperTests

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+  name: "WhisperTesting",
+  products: [],
+  dependencies: [
+    .package(url: "https://github.com/ggerganov/whisper.spm", from: "1.0.0"),
+  ],
+  targets: [
+    .testTarget(
+      name: "WhisperTests",
+      dependencies: [
+        .product(name: "whisper", package: "whisper.spm"),
+      ],
+      resources: [
+        .copy("models"),
+      ]
+    ),
+  ]
+)
